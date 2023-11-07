c04eccabd757627d733e18502c9758b1c6b65fbc
Gzuie <sara.maheo@outlook.fr>
2023-11-07 16:12:55 +0100

    init

---
+++ src/Form/SortieType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('dateHeureFin')
+            ->add('dateLimiteInscription')
+            ->add('nombreMaxParticipant')
+            ->add('description')
+            ->add('isPublish')
+            ->add('motif')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
