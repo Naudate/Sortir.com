3c1e34d76d03d2146acaad94a3b7cdc687efff55
lucie fribault <lucie.frib@live.fr>
2023-11-13 16:12:04 +0100

    Presque le tableau d'affichage des sorties

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -13,11 +14,33 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(
+        Request $request,
         EntityManagerInterface $entityManager,
         SortieRepository $sortieRepository)
     {
-        $sorties = $sortieRepository->findAll();
+        /*$sorties = $sortieRepository->findAll();
         dump($sorties);
-        return $this->render('home/index.html.twig', ["sorties" => $sorties]);
+        return $this->render('home/index.html.twig', ["sorties" => $sorties]);*/
+
+        $searchInput = $request->query->get('nom');
+        $dateDebut = $request->query->get('dateDebut');
+        $dateFin = $request->query->get('dateFin');
+        $organisateurOnly = $request->query->get('organisateur');
+        $voirSortiesPassees = $request->query->get('voirSortiesPassees');
+
+        // Filtrer les sorties en fonction des dates
+        $sorties = $sortieRepository->findBetweenDates($dateDebut, $dateFin, $searchInput, $organisateurOnly, $this->getUser());
+        if ($voirSortiesPassees) {
+            $sorties = array_filter($sorties, function ($sortie) {
+                return $sortie->getDateHeureDebut() < new \DateTime();
+            });
+        }
+        return $this->render('home/index.html.twig', [
+
+            "sorties" => $sorties,
+            "dateDebut" => $dateDebut,
+            "dateFin" => $dateFin,
+            "organisateurOnly" => $organisateurOnly,
+        ]);
     }
 }
